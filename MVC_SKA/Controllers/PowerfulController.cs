000fb9f9dc68ea0c9f04f4c9ac8c1cc137544eda
vincent.chen99 <vincent.chen99@TI03NB68>
2019-03-08 16:14:40 +0800

    Finished

---
+++ MVC_SKA/Controllers/PowerfulController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_SKA.Models;
+
+namespace MVC_SKA.Controllers
+{
+    public class PowerfulController : Controller
+    {
+        private Random _rnd = new Random();
+        // GET: Powerful
+        public ActionResult MoneyTemplate()
+        {
+            List<ViewModel> LST = new List<ViewModel>(50);
+
+            for (int x = 1; x < 51; x++)
+            {
+
+
+                var model = new ViewModel()
+                {
+                    ID = x,
+                    TYPE = rnT(),
+                    DATE = rnD(),
+                    PRICE = rnP()
+                };
+                LST.Add(model);
+            }
+
+
+
+
+            return View(LST);
+        }
+        public string rnD()
+        {
+
+            int range = 1 * 365; //5 years   
+
+            String randomDate = DateTime.Today.AddDays(-_rnd.Next(range)).ToShortDateString();
+            return randomDate;
+        }
+
+        public int rnP()
+        {
+            int x = _rnd.Next(1000, 20001);
+            return x;
+
+        }
+
+        public String rnT()
+        {
+            int x = _rnd.Next(0, 2);
+            string t;
+            if (x == 0)
+                t = "支出";
+
+            else
+                t = "收入";
+
+            return t;
+
+        }
+    }
+}
---
+++ MVC_SKA/Models/ViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_SKA.Models
+{
+    public class ViewModel
+    {
+        public int ID { get; set; }
+
+        public string TYPE { get; set; }
+
+        public string DATE { get; set; }
+
+        public int PRICE { get; set; }
+    }
+}
